12b61f771ff7753225a6db3b6e5ed0e98cdc19bb
Carl Fairclough <contact@carlfairclough.me>
2015-09-22 18:58:21 +0700

    remove p tags around images

--- library/bones.php
+++ library/bones.php
@@ -254,7 +254,9 @@ RANDOM CLEANUP ITEMS
 *********************/
 
 // remove the p from around imgs (http://css-tricks.com/snippets/wordpress/remove-paragraph-tags-from-around-images/)
-
+function bones_filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
 
 // This removes the annoying […] to a Read More link
 function bones_excerpt_more($more) {
